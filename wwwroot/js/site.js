fa4d9d264a192fd13e89afb83a7bf2c27992aae2
vifa <vifa2201@student.miun.se>
2024-01-23 12:23:34 +0100

    first commit

---
+++ wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿
+
+// Funktion för att sortera lista
+function sortCourses() {
+    // Hämtar listan
+    const courseList = document.getElementById("courseList");
+
+    // Hämtar list items och konverterar dem till en array
+    let items = Array.from(courseList.getElementsByTagName("li"));
+
+    // Sortera arrayen av list items baserat på deras textinnehåll
+    items.sort((a, b) => a.textContent.localeCompare(b.textContent));
+
+    // Rensa listan
+    courseList.innerHTML = "";
+
+    // Lägg till de sorterade list items tillbaka i listan
+    items.forEach(item => courseList.appendChild(item));
+}
